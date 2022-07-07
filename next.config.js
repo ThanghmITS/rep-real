34ec9df05696601c30b1daa63c58f658b95196a8
ThanghmITS <thanghm@its-global.vn>
2022-07-07 21:34:50 +0700

    base project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    dirs: ['src'],
+  },
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ['en', 'vi'],
+    defaultLocale: 'en',
+  },
+  trailingSlash: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
