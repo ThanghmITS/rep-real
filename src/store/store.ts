4d94d27ba06350e113582ea8b9a03c4d031e2550
ThanghmITS <thanghm@its-global.vn>
2022-07-11 14:54:02 +0700

    call api

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '@/pages/count/counterSlice'
+import counterReducer from '@/pages/count/counterSlice';
+import postReducer from '@/redux/slices/postSlice';
 export const store = configureStore({
-    reducer: {
-        counter: counterReducer,
-    },
+  reducer: {
+    counter: counterReducer,
+    post: postReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
--- src/utils/Http-request.ts
+++ src/utils/Http-request.ts
@@ -1,4 +1,3 @@
-
 import { ACCESS_TOKEN } from '@/constants/Storage';
 import storage from '@/utils/storage';
 import axios from 'axios';
@@ -19,7 +18,7 @@ axiosClient.interceptors.request.use(
   },
   function (error) {
     return Promise.reject(error);
-  }
+  },
 );
 
 axiosClient.interceptors.response.use(
@@ -29,7 +28,7 @@ axiosClient.interceptors.response.use(
   async function (error) {
     const originalConfig = error.config;
     if (originalConfig.url !== '/login' && error.response) {
-      if (error.response.status === 401) {
+      if ([401, 403].includes(Number(error.response.status))) {
         if (originalConfig.retry) {
           removeAll();
           return location('/login');
@@ -41,7 +40,7 @@ axiosClient.interceptors.response.use(
       alert('Please check your internet connection and try again');
     }
     return Promise.reject(error);
-  }
+  },
 );
 
 export default axiosClient;
